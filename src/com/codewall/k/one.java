e567b09f665493839865ec2ca86b0dc71623974c
Zynn <zynnk21316@gmail.com>
2022-06-15 19:03:00 +0630

    part one lesson1

---
+++ src/com/codewall/k/one.java
@@ -0,0 +1,12 @@
+package com.codewall.k;
+
+public class one {
+    public static void main(String[] args) {
+        boolean sk = 3==4;
+        System.out.println(sk);
+
+        boolean sl = 3>6 || 4>5;
+        System.out.println(sl);
+        System.out.println(sk + "zoro");
+    }
+}
